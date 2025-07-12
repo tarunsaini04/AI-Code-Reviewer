1e3f768ad584652bd9cf004b5ddf770732e310ae
tarunsaini04 <151149190+tarunsaini04@users.noreply.github.com>
2025-07-13 01:17:35 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   })
 
    async function reviewCode(){
-     const response = await axios.post('http://localhost:3000/ai/get-review',{code}) 
+     const response = await axios.post('https://ai-code-reviewer-backend-yggx.onrender.com/ai/get-review',{code}) 
      setReview(response.data)
    }
 
